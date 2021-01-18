63cb03324f116f5dfa99603e25dae94c72955521
DeeDeeAich <32936305+DeeDeeAich@users.noreply.github.com>
2021-01-18 12:45:50 -0600

    Create main, test json, and update gitignore

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+user_choice = input("Please enter \"new\" if you\'d like to add a new user. Please enter \"info\" if you'd like to check a previous user\'s information.")
+
+class AddressBook:
+    def __init__(self):
+        pass
+
+    def get_info(self):
+        input("First name: ")
+
